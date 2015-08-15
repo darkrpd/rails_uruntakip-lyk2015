1181348acb45777112221e68dab9c0a4e1a04988
darkrpd <aliaskin@outlook.com>
2015-08-15 10:02:57 +0300

    v0.3

--- db/migrate/20150814230435_add_product_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :product_id , :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814230435) do
+ActiveRecord::Schema.define(version: 20150815065147) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150814230435) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
