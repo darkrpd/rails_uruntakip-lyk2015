c66f6924a7772918ffb3667328623bb470585a7e
darkrpd <aliaskin@outlook.com>
2015-08-16 11:46:15 +0300

    v0.6- nested form eklendi...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,26 @@ class UsersController < ApplicationController
 
     def index
       @users = User.all
-
     end
 
         def new
+          # user_params= params.require(:user).permit(:username, :email,:age,:city)
+           @nested = User.new
 
         end
 
 
         def create
-          user_params= params.require(:user).permit(:username, :email,:age,:city)
-           @user = User.new(user_params)
-           @user.save
-       redirect_to @user
+          # user_params= params.require(:user).permit(:username, :email,:age,:city)
+
+            user_params= params.require(:user).permit(:username, :email,:age,:city,
+            products_attributes: [:title, :description, :price])
+            @nested = User.new(user_params)
+            @nested.save
+          #  @user = User.new(user_params)
+          #  @user.save
+       redirect_to users_path
+
         end
 
         def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   ##has_one :product , foreign_key: 'product_id', dependent: :destroy
   has_many :product , dependent: :destroy
+  accepts_nested_attributes_for :product
 
   validates :username ,presence: true, length: { in: 6..20,message: "username"  }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ ,
